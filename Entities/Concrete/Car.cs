09141d5e88bdbee06c574b7691d0575b44f81027
Sergen Bakır <sbakir09@outlook.com>
2021-03-27 17:03:11 +0300

    27.03 Updated

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -8,7 +8,7 @@ namespace Entities.Concrete
 {
     public class Car:IEntity
     {
-        [Key]
+        
         public int CarId { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
--- Entities/Concrete/Rental.cs
+++ Entities/Concrete/Rental.cs
@@ -8,7 +8,7 @@ namespace Entities.Concrete
 {
     public class Rental:IEntity
     {
-        [Key]
+        
         public int RentId { get; set; }
         public int CarId { get; set; }
         public int CustomerId { get; set; }
